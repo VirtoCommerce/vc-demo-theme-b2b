ab7400e682b01bb06b2131020cd674e4976a557f
Ilya <ilya.ivanov6@gmail.com>
2020-09-11 12:13:26 +0200

    VP-4636: Add "Current user roles" widget to dashboard

---
+++ assets/js/account/dashboard-user-roles-list.js
@@ -0,0 +1,32 @@
+angular.module('storefront.account')
+    .component('vcDashboardUserRolesList', {
+        templateUrl: "dashboard-user-roles-list.tpl",
+        controller: ['accountApi', 'loadingIndicatorService', function (accountApi, loader) {
+            var $ctrl = this;
+            $ctrl.loader = loader;
+
+            function refresh() {
+                loader.wrapLoading(function () {
+                    return accountApi.searchOrganizationUsers({
+                        skip: 0,
+                        take: 100
+                    }).then(function (response) {
+                        $ctrl.userEntries = response.data.results;
+                        const roleRecords = $ctrl.userEntries.map(x => ({
+                            id: x.role.id,
+                            name: x.role.name,
+                            description: x.role.description
+                        }));
+                        $ctrl.distinctRoles = unique(roleRecords, 'id');
+                    });
+                });
+            };
+
+            function unique(array, propertyName) {
+                return array.filter((e, i) => array.findIndex(a => a[propertyName] === e[propertyName]) === i);
+             }
+
+            refresh();
+
+        }]
+    });
