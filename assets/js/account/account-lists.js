5de204e81e6d8b49e2cbe3614ecba9613797e91c
trueboroda <superiorboroda@gmail.com>
2020-09-17 15:46:00 +0300

    VP-4287: Add disabling for “Add to cart" button to wish lists at...
    unavailable and sold out cases

--- assets/js/account/account-lists.js
+++ assets/js/account/account-lists.js
@@ -129,6 +129,15 @@
                          return lineItem.product.isBuyable && lineItem.product.isInStock;
                     }
 
+                    $ctrl.addToCartAllProductsEnabled() {
+                        var result = $ctrl.accountLists.selectedList.items.some(function (item) {
+                            return  $ctrl.canBeAddedToCart(item);
+                        });
+
+                        return result;
+                    }
+
+
                     $ctrl.addToCartAllProducts = function (listName) {
                         loader.wrapLoading(function () {
                             return listsApi.mergeWithCurrentCart(listName, $ctrl.type).then(function (response) {
