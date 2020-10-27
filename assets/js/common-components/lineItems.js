022f1c403ba73b39c4a4627ace9d9359aac6c26b
Ilya <ilya.ivanov6@gmail.com>
2020-10-27 17:48:48 +0200

    VP-5696: Show configurable products in order details template

--- assets/js/common-components/lineItems.js
+++ assets/js/common-components/lineItems.js
@@ -3,13 +3,33 @@
 storefrontApp.component('vcLineItems', {
     templateUrl: "themes/assets/js/common-components/lineItems.tpl.liquid",
     bindings: {
-        items: '='
+        order: '='
     },
     controller: ['$scope', function ($scope) {
+        var $ctrl = this;
 
-        $scope.addProductToCart = function (productId, quantity) {
+        $ctrl.addProductToCart = function (productId, quantity) {
             $scope.$emit('lineItemAdded', {productId, quantity});
         }
+
+        $ctrl.getToggleTitle = function (group) {
+            return group.showConfiguration === false ? 'Show configuration' : 'Hide configuration';
+        };
+
+        $ctrl.toggleConfiguration = function(group) {
+            group.showConfiguration = !group.showConfiguration;
+        };
+
+        function getConfiguredLineItems(groups) {
+            _.each(groups, group => {
+                angular.extend(group, { showConfiguration: false });
+                _.each(group.parts, part => {
+                    part.items = [group.items.find(x => x.id === part.selectedItemId)];
+                });
+            });
+        }
+
+        getConfiguredLineItems($ctrl.order.configuredGroups);
 
     }]
 });
