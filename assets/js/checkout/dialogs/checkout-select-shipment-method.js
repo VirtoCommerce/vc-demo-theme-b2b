2f75fef745e91150164ad927acc63cc4c8ec43ac
Ilia <ilya.ivanov6@gmail.com>
2020-11-02 12:55:25 +0200

    Count total for shipment method popup

--- assets/js/checkout/dialogs/checkout-select-shipment-method.js
+++ assets/js/checkout/dialogs/checkout-select-shipment-method.js
@@ -10,6 +10,10 @@ angular.module('storefrontApp')
         $scope.checkout.shipmentMethod = shipmentMethod;
     }
 
+    $scope.getTotal = function (shippingMethod) {
+        return shippingMethod.price.amount === 0 ? 'Free' : shippingMethod.price.formattedAmount;
+    }
+
     $scope.close = function (result) {
         if (result) {
             $uibModalInstance.close(result);
