1395a393a357c636611e2175324d28e326dad8fd
trueboroda <superiorboroda@gmail.com>
2020-12-29 13:36:26 +0300

    Add and use only-integer direvtive
    For text input where integer value is set

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -67,33 +67,6 @@ storefrontApp.factory('roundHelper', function () {
     }
 });
 
-storefrontApp.factory('validationHelper', function () {
-    return {
-        positiveInt: function($event) {
-            const e = $event;
-
-            if ([46, 8, 9, 27, 13].indexOf(e.keyCode) !== -1 ||
-                // Allow: Ctrl+A
-                (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
-                // Allow: Ctrl+C
-                (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) ||
-                // Allow: Ctrl+V
-                (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) ||
-                // Allow: Ctrl+X
-                (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
-                // Allow: home, end, left, right
-                (e.keyCode >= 35 && e.keyCode <= 39)) {
-              // let it happen, don't do anything
-              return;
-            }
-            // Ensure that it is a number and stop the keypress
-            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-              e.preventDefault();
-            }
-        }
-    }
-});
-
 storefrontApp.config(['$httpProvider', 'locale', 'localization', '$translateProvider', 'vcRecaptchaServiceProvider', 'reCaptchaKey', function ($httpProvider, locale, localization, $translateProvider, vcRecaptchaServiceProvider, reCaptchaKey) {
     //$locationProvider.html5Mode({ enabled: true, requireBase: false, rewriteLinks: false });
     $httpProvider.interceptors.push('httpErrorInterceptor');
