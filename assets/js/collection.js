04a32120e58edf8d256840d9025ab48b684024f2
trueboroda <superiorboroda@gmail.com>
2021-01-21 13:49:33 +0300

    Fix error. catalog - filters - id instead of text when select branch availability

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -1,8 +1,10 @@
 var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('collectionController', ['$scope', '$location',  function ($scope, $location) {
+storefrontApp.controller('collectionController', ['$scope', '$location', 'inventoryApi',  function ($scope, $location, inventoryApi) {
     $scope.currentFulfillmentCenter = { id: null };
     var $ctrl = this;
+    $scope.fulfillmentCenters = [];
+
     $ctrl.init = function() {
         $ctrl.sortModes = {
             'priority-descending;name-ascending': 'collections.sorting.featured',
@@ -16,12 +18,34 @@ storefrontApp.controller('collectionController', ['$scope', '$location',  functi
         };
         $ctrl.viewQuery = { view: ['grid'] };
         $ctrl.keywordQuery = { keyword: [] };
+        loadFulfillmentCenters();
     }
 
     $ctrl.generatePageSizes = function (capacity, steps) {
         $ctrl.pageSizeQuery = { page_size: [capacity] };
         // for example            start: 16 stop: 16 * 3 + 1 = 49 step: 16
         $ctrl.pageSizes = _.range(capacity, capacity * steps + 1, capacity);
+    }
+
+    function loadFulfillmentCenters () {
+        inventoryApi.searchFulfillmentCenters({ }).then(function(response) {
+             $scope.fulfillmentCenters = response.data.results;
+         });
+    }
+
+    $scope.adaptFilterName = function (termKey) {
+      return termKey == 'available_in' ? 'Available in' : termKey;
+    }
+
+    $scope.adaptFilterValue = function (termKey, termValue) {
+      let resultValue = termValue;
+
+      if (termKey == 'available_in') {
+        const fulfillmentCenter = $scope.fulfillmentCenters.find(x=>x.id === termValue);
+        resultValue = !!fulfillmentCenter ? fulfillmentCenter.name : '';
+      }
+
+      return resultValue;
     }
 
 
