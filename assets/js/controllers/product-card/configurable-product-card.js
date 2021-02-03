4b9fd3c392589ecc7ab81aa667c2a2968d7dd636
Ilia <ilya.ivanov6@gmail.com>
2021-02-03 16:34:17 +0200

    Integrate configurable backend to configurable product card

--- assets/js/controllers/product-card/configurable-product-card.js
+++ assets/js/controllers/product-card/configurable-product-card.js
@@ -1,22 +1,23 @@
 var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('configurableProductCardController', ['$scope', 'catalogService', '$filter', 'roundHelper', 'storeCurrency',
-    function ($scope, catalogService, $filter, roundHelper, storeCurrency) {
+storefrontApp.controller('configurableProductCardController', ['$scope', 'catalogService', '$filter', 'storeCurrency',
+    function ($scope, catalogService, $filter, storeCurrency) {
 
         $scope.getDefaultPrice = function() {
             return $filter('currency')($scope.defaultPrice, storeCurrency.symbol);
         }
 
         $scope.initProductConfiguration = function(productId) {
-            catalogService.getProductConfiguration(productId).then(function(response) {
-                $scope.productParts = response.data;
-                $scope.defaultProductParts = [];
-                _.each($scope.productParts, function (part) {
-                    $scope.defaultProductParts.push(part.items.find(x => x.id === part.selectedItemId));
-                });
-
-                $scope.defaultPrice = roundHelper.bankersRound($scope.defaultProductParts.reduce((prev, cur) => prev + cur.price.actualPrice.amount, 0));
-            });
+            catalogService.getProduct([productId]).then(response => {
+              let defaultPartsTotalsObject = [];
+              $scope.productParts = response.data[0].parts;
+              _.each($scope.productParts, part => {
+                  defaultPartsTotalsObject.push({id: part.items.find(x => x.id === part.selectedItemId).id, quantity: 1});
+              });
+              pricingService.getProductsTotal(defaultPartsTotalsObject).then(result => {
+                $scope.defaultPrice = $scope.showPricesWithTaxes ? result.data.totalWithTax.amount : result.data.total.amount;
+              });
+          });
         }
 
     }]);
