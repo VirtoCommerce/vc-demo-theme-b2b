2d5236165dc33c65a9746ad2c6acdbe3b8ef7525
Anton Veretennikov <superiorboroda@gmail.com>
2021-02-18 17:20:56 +0300

    use others templates for b4 acconunt manager template and for old

---
+++ assets/js/account/account-manager.js
@@ -0,0 +1,26 @@
+angular.module('storefront.account')
+    .component('vcAccountManager', {
+        templateUrl: [ '$rootScope', function($rootScope) {
+                return $rootScope.adjustTemplateUrl("/themes/assets/js/account/account-manager.tpl");
+        } ],
+        bindings: {
+            baseUrl: '<',
+            customer: '<'
+        },
+        controller: ['$scope', '$timeout', 'storefrontApp.mainContext', 'loadingIndicatorService', 'commonService', function ($scope, $timeout, mainContext, loader, commonService) {
+            var $ctrl = this;
+            $ctrl.loader = loader;
+            $ctrl.availCountries = [];
+            loader.wrapLoading(function () {
+                return commonService.getCountries().then(function (response) {
+                    $ctrl.availCountries = response.data;
+                });
+            });
+
+            $ctrl.getCountryRegions = function (country) {
+                return loader.wrapLoading(function () {
+                    return commonService.getCountryRegions(country.code3).then(function (response) { return response.data; });
+                });
+            };
+        }]
+    });
