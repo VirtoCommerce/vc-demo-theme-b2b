bf9c00e1f46dca3cd5fc7d3cd5e8058b776ade77
Ilia <ilya.ivanov6@gmail.com>
2021-02-24 09:19:21 +0200

    Fix 'add-to-compare-checkbox' formatting

--- assets/js/product-compare/add-to-compare-checkbox.js
+++ assets/js/product-compare/add-to-compare-checkbox.js
@@ -4,38 +4,41 @@ function addToCompareCheckboxController($rootScope, $scope, catalogService, comp
   $ctrl.containProduct = false;
 
   $ctrl.$onInit = function () {
-      $ctrl.containProduct = compareProductService.isInProductCompareList($ctrl.productId);
-  }
+      $ctrl.containProduct = compareProductService.isInProductCompareList(
+          $ctrl.productId
+      );
+  };
 
   $ctrl.addProductToCompareList = function (event) {
       event.preventDefault();
-      catalogService.getProduct($ctrl.productId).then(function(response) {
+      catalogService.getProduct($ctrl.productId).then(function (response) {
           var product = response.data[0];
           event.preventDefault();
-          var isInProductList = compareProductService.isInProductCompareList($ctrl.productId);
+          var isInProductList = compareProductService.isInProductCompareList(
+              $ctrl.productId
+          );
           if (!isInProductList) {
               var count = compareProductService.getProductsCount();
               if (count < 5) {
-              $ctrl.containProduct = true;
-              compareProductService.addProduct($ctrl.productId);
-              $rootScope.$broadcast('productCompareListChanged');
+                  $ctrl.containProduct = true;
+                  compareProductService.addProduct($ctrl.productId);
+                  $rootScope.$broadcast("productCompareListChanged");
               }
-          }
-          else {
+          } else {
               $ctrl.containProduct = false;
               compareProductService.removeProduct($ctrl.productId);
-              $rootScope.$broadcast('productCompareListChanged');
+              $rootScope.$broadcast("productCompareListChanged");
           }
-      })
+      });
   };
 
-  $scope.$on('productRemovedFromCompareList', function(event, data) {
+  $scope.$on("productRemovedFromCompareList", function (event, data) {
       if ($ctrl.productId == data) {
           $ctrl.containProduct = false;
       }
   });
 
-  $scope.$on('productCompareListCleared', function(event, data) {
+  $scope.$on("productCompareListCleared", function (event, data) {
       $ctrl.containProduct = false;
   });
 }
